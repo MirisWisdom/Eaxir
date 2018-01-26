bc2b2f529a46a8400abf97bef73f000e892fe42e
Emilian Roman <roman.emilian@outlook.com>
2018-01-26 23:45:25 +0800

    Add comments for the EaxPackage methods

--- Eaxir.Library/EaxPackage.cs
+++ Eaxir.Library/EaxPackage.cs
@@ -2,23 +2,40 @@
 
 namespace Eaxir.Library
 {
+    /// <summary>
+    /// Handles the (un)installation of the EAX library.
+    /// </summary>
     public class EaxPackage : IInstall, IUninstall
     {
+        /// <summary>
+        /// Automatically install the EAX to the HCE directory.
+        /// </summary>
         public void Install()
         {
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Install the EAX to a specified HCE directory.
+        /// </summary>
+        /// <param name="path"></param>
         public void Install(string path)
         {
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Automatically uninstall the EAX from the HCE directory.
+        /// </summary>
         public void Uninstall()
         {
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Uninstall teh EAX from a specified HCE directory.
+        /// </summary>
+        /// <param name="path"></param>
         public void Uninstall(string path)
         {
             throw new System.NotImplementedException();
