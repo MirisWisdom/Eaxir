332b907fe78504c3481836bd856582f3ae992d51
Emilian Roman <roman.emilian@outlook.com>
2018-01-26 23:19:05 +0800

    Define IUninstall Interface

---
+++ Eaxir.Library/Interfaces/IUninstall.cs
@@ -0,0 +1,19 @@
+﻿namespace Eaxir.Library.Interfaces
+{
+    /// <summary>
+    /// Interface for exposing methods that handle uninstallation of packages.
+    /// </summary>
+    public interface IUninstall
+    {
+        /// <summary>
+        /// Uninstall the package from its default location.
+        /// </summary>
+        void Uninstall();
+
+        /// <summary>
+        /// Uninstall the package from a custom location.
+        /// </summary>
+        /// <param name="path"></param>
+        void Uninstall(string path);
+    }
+}
