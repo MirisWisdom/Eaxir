ed5f0683f30322a1ea90c6b2162183e3cf7359c5
Emilian Roman <roman.emilian@outlook.com>
2018-01-26 23:05:00 +0800

    Define the EaxPackage class

---
+++ Eaxir.Library/EaxPackage.cs
@@ -0,0 +1,17 @@
+﻿using Eaxir.Library.Interfaces;
+
+namespace Eaxir.Library
+{
+    public class EaxPackage : IInstall
+    {
+        public void Install()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Install(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
